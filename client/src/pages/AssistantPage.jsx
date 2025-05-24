0fe7eb297fd27abd0289fcd883d690de5d9ab6c1
Vishisht Dixit <vishishtdixit23@gmail.com>
2025-05-24 12:15:33 +0530

    deleted extra files

--- client/src/pages/AssistantPage.jsx
+++ client/src/pages/AssistantPage.jsx
@@ -22,7 +22,7 @@ function AssistantPage() {
 
     try {
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyDvupRZgF_7x1xjN1UdgedeE-9vkabyRE0`,
         {
           method: "POST",
           headers: {
